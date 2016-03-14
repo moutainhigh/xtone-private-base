c85496adbca9ef0764297536e3c12a9ad54f888a
shotgun <cbanor@21cn.com>
2016-03-14 14:44:22 +0800

    垫付平台同步回调

---
+++ HtProject-sp-callback/src/com/dianfu/logical/CPTronePusher.java
@@ -0,0 +1,106 @@
+package com.dianfu.logical;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.database.LightModel.tbl_trone_orderItem;
+import com.dianfu.Interface.ICPPushModel.EPushField;
+import com.shotgun.Tools.Funcs;
+
+public class CPTronePusher extends BasePusher {
+
+	private ArrayList<tbl_trone_orderItem> _allCfg;
+
+	@Override
+	public boolean LoadCPAPI(int trondId) {
+		// var l = LightDataModel.tbl_trone_orderItem.GetQueries(dBase);
+		// l.Filter.AndFilters.Add(LightDataModel.tbl_trone_orderItem.Fields.trone_id,
+		// TroneId);
+		// l.Filter.AndFilters.Add(LightDataModel.tbl_trone_orderItem.Fields.disable,
+		// 0);
+		// l.PageSize = int.MaxValue;
+		if (trondId == 0) {
+			if (getTrone() == null)
+				return SetError("trondId 不能为空");
+			trondId = getTrone().get_id();
+		}
+
+		String sql = "select * from tbl_trone_order where disable=0 and trone_id=" + Integer.toString(trondId);
+
+		_allCfg = super.getDBase().sqlToModels(new tbl_trone_orderItem(), sql);
+
+		if (_allCfg == null || _allCfg.size() == 0) {
+			tbl_trone_orderItem m = CreateDefaultTrone();
+			_allCfg = new ArrayList<tbl_trone_orderItem>();
+			_allCfg.add(m);
+		}
+		// base.SetConfig(_allCfg[0]);
+		return super.SetSuccess();
+
+	}
+
+	private tbl_trone_orderItem CreateDefaultTrone() {
+		tbl_trone_orderItem ret = new tbl_trone_orderItem();
+		ret.set_cp_id(34); // 未知CP的ID
+		ret.set_create_date(new Date());
+		ret.set_disable(false);
+		ret.set_is_dynamic(false);
+		ret.set_is_unknow(true);
+		ret.set_order_num("*");
+		ret.set_order_trone_name("未分配指令");
+		ret.set_push_url_id(47); // 未知CP推送URL
+		ret.set_trone_id(getTrone().get_id());
+		getDBase().saveData(ret);
+		return ret;
+	}
+
+
+	@Override
+	public boolean DoPush() {
+		boolean isRecord = false;
+		tbl_trone_orderItem defCfg = null;
+		if (getPushObject().get_cp_id() > 0 && getPushObject().get_cp_id() != 34)
+			return true;
+		for (tbl_trone_orderItem m : _allCfg) {
+			if (m.get_is_unknow()) {
+				if (defCfg != null)
+					super.WriteLog(-3, "存在多个默认CP！cfgId:" + Integer.toString(m.get_id()));
+				defCfg = m;
+				continue;
+			}
+			if (!IsMatch(m))
+				continue;
+			if (isRecord) {
+				super.WriteLog(-3, "配置有冲突：cfgId:" + Integer.toString(m.get_id()));
+				continue;
+			}
+			isRecord = true;
+			SetConfig(m);
+			super.DoPush();
+		}
+		if (isRecord)
+			return true;
+		if (getPushObject().get_cp_id() == 34)
+			return true;
+		if (defCfg == null)
+			defCfg = CreateDefaultTrone();
+		SetConfig(defCfg);
+		return super.DoPush();
+	}
+
+	private boolean IsMatch(tbl_trone_orderItem m) {
+		String spMsg = getPushObject().GetValue(EPushField.Msg);
+		if (spMsg == null)
+			spMsg = "";
+		if (m.get_is_dynamic()) {// CP可模糊的指令
+			Pattern rx = Funcs.genericToRegx(m.get_order_num());
+			Matcher mc = rx.matcher(spMsg);
+			return mc.find();
+		}
+		// CP精确指令
+		return spMsg.equalsIgnoreCase(m.get_order_num());
+	}
+
+}
