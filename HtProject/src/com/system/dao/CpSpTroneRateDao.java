406b96af30e5bf84f5abb7fe6bf901ccb9b3acf7
Andy.Chen <chenls@bjxiangtone.com>
2016-03-17 13:25:01 +0800

    增加CP对SP业务的结算率处理

--- HtProject/src/com/system/dao/CpSpTroneRateDao.java
+++ HtProject/src/com/system/dao/CpSpTroneRateDao.java
@@ -1,6 +1,129 @@
 package com.system.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.system.constant.Constant;
+import com.system.database.JdbcControl;
+import com.system.database.QueryCallBack;
+import com.system.model.CpSpTroneRateModel;
+import com.system.util.StringUtil;
+
 public class CpSpTroneRateDao
 {
-
+	public Map<String, Object> loadCpSpTroneRate(String keyWord,int pageIndex)
+	{
+		String sql = "select " + Constant.CONSTANT_REPLACE_STRING + " FROM daily_config.`tbl_cp_trone_rate` a ";
+		
+		sql += " LEFT JOIN daily_config.`tbl_cp` b ON a.`cp_id` = b.`id`";
+		sql += " LEFT JOIN daily_config.`tbl_sp_trone` c ON a.`sp_trone_id` = c.`id`";
+		sql += " LEFT JOIN daily_config.`tbl_sp` d ON c.`sp_id` = d.`id`";
+		sql += " WHERE 1=1";
+		
+		if(StringUtil.isNullOrEmpty(keyWord))
+		{
+			sql += " AND (b.`short_name` LIKE '%" + keyWord + "%' OR b.`full_name` LIKE '%" + keyWord + "%' ";
+			sql += " OR c.`name` LIKE '%" + keyWord + "%' OR d.`short_name` LIKE '%" + keyWord + "%' OR d.`full_name` LIKE '%" + keyWord + "%')";
+		}
+		
+		String limit = " limit "  + Constant.PAGE_SIZE*(pageIndex-1) + "," + Constant.PAGE_SIZE;
+		
+		sql += " order by a.id desc ";
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		JdbcControl control = new JdbcControl();
+		
+		map.put("rows",control.query(sql.replace(Constant.CONSTANT_REPLACE_STRING, "count(*)"), new QueryCallBack()
+		{
+			@Override
+			public Object onCallBack(ResultSet rs) throws SQLException
+			{
+				if(rs.next())
+					return rs.getInt(1);
+				
+				return 0;
+			}
+		}));
+		
+		map.put("list", control.query(sql.replace(Constant.CONSTANT_REPLACE_STRING, " a.*,b.`short_name` cp_name,c.`name` sp_trone_name,d.`id` sp_id,d.`short_name` sp_name ") + limit, new QueryCallBack()
+		{
+			@Override
+			public Object onCallBack(ResultSet rs) throws SQLException
+			{
+				List<CpSpTroneRateModel> list = new ArrayList<CpSpTroneRateModel>();
+				
+				while(rs.next())
+				{
+					CpSpTroneRateModel model = new CpSpTroneRateModel();
+					
+					model.setId(rs.getInt("id"));
+					model.setCpId(rs.getInt("cp_id"));
+					model.setSpTroneId(rs.getInt("sp_trone_id"));
+					model.setRate(rs.getFloat("rate"));
+					model.setSpName(StringUtil.getString(rs.getString("sp_name"), ""));
+					model.setCpName(StringUtil.getString(rs.getString("cp_name"), ""));
+					model.setSpTroneName(StringUtil.getString(rs.getString("sp_trone_name"), ""));
+					
+					list.add(model);
+				}
+				
+				return list;
+			}
+		}));
+		
+		return map;
+	}
+	
+	public CpSpTroneRateModel loadCpSpTroneRateById(int id)
+	{
+		String sql = "select a.*,b.`short_name` cp_name,c.`name` sp_trone_name,d.`id` sp_id,d.`short_name` sp_name FROM daily_config.`tbl_cp_trone_rate` a ";
+		
+		sql += " LEFT JOIN daily_config.`tbl_cp` b ON a.`cp_id` = b.`id`";
+		sql += " LEFT JOIN daily_config.`tbl_sp_trone` c ON a.`sp_trone_id` = c.`id`";
+		sql += " LEFT JOIN daily_config.`tbl_sp` d ON c.`sp_id` = d.`id`";
+		sql += " WHERE a.id = " + id;
+		
+		return (CpSpTroneRateModel)new JdbcControl().query(sql, new QueryCallBack()
+		{
+			@Override
+			public Object onCallBack(ResultSet rs) throws SQLException
+			{
+				if(rs.next())
+				{
+					CpSpTroneRateModel model = new CpSpTroneRateModel();
+					
+					model.setId(rs.getInt("id"));
+					model.setCpId(rs.getInt("cp_id"));
+					model.setSpTroneId(rs.getInt("sp_trone_id"));
+					model.setRate(rs.getFloat("rate"));
+					model.setSpName(StringUtil.getString(rs.getString("sp_name"), ""));
+					model.setCpName(StringUtil.getString(rs.getString("cp_name"), ""));
+					model.setSpTroneName(StringUtil.getString(rs.getString("sp_trone_name"), ""));
+					
+					return model;
+				}
+				return null;
+			}
+		});
+	}
+	
+	public void addCpSpTroneRate(CpSpTroneRateModel model)
+	{
+		String sql = "";
+	}
+	
+	public void updateCpSpTroneRate(CpSpTroneRateModel model)
+	{
+		
+	}
+	
+	public void delCpSpTroneRate(int id)
+	{
+		
+	}
 }
--- HtProject/src/com/system/model/CpSpTroneRateModel.java
+++ HtProject/src/com/system/model/CpSpTroneRateModel.java
@@ -8,6 +8,73 @@ public class CpSpTroneRateModel
 	private int spId;
 	private String spName;
 	private int spTroneId;
-	private int spTroneName;
+	private String spTroneName;
+	private float rate;
+	
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public int getCpId()
+	{
+		return cpId;
+	}
+	public void setCpId(int cpId)
+	{
+		this.cpId = cpId;
+	}
+	public String getCpName()
+	{
+		return cpName;
+	}
+	public void setCpName(String cpName)
+	{
+		this.cpName = cpName;
+	}
+	public int getSpId()
+	{
+		return spId;
+	}
+	public void setSpId(int spId)
+	{
+		this.spId = spId;
+	}
+	public String getSpName()
+	{
+		return spName;
+	}
+	public void setSpName(String spName)
+	{
+		this.spName = spName;
+	}
+	public int getSpTroneId()
+	{
+		return spTroneId;
+	}
+	public void setSpTroneId(int spTroneId)
+	{
+		this.spTroneId = spTroneId;
+	}
+	public String getSpTroneName()
+	{
+		return spTroneName;
+	}
+	public void setSpTroneName(String spTroneName)
+	{
+		this.spTroneName = spTroneName;
+	}
+	public float getRate()
+	{
+		return rate;
+	}
+	public void setRate(float rate)
+	{
+		this.rate = rate;
+	}
+	
 	
 }
