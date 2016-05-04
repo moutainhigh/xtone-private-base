c8e18013d3e7a92c354c55f7b97345f0e0848b0d
berlin <285184329@qq.com>
2016-05-04 09:53:35 +0800

    万家游戏银联支付服务端

--- xtone-thirdpay-webhook/src/main/com/thirdpay/servlet/BaidupayCountServlet.java
+++ xtone-thirdpay-webhook/src/main/com/thirdpay/servlet/BaidupayCountServlet.java
@@ -19,6 +19,13 @@ import org.common.util.ThreadPool;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.thirdpay.domain.PayInfoBean;
+<<<<<<< master
+=======
+import com.thirdpay.utils.payConstants;
+/**
+ * 参数名 参数含义 格式说明 长度 是否必须 sp_no 百度钱包支付商户号 10位数字组成的字符串  是 order_no 商户订单号 不超过20个字符 Max(20) 是 bfb_order_no 百度钱包支付交易号 不超过32个字符 Max(32) 是 bfb_order_create_time 百度钱包支付交易创建 时间 YYYYMMDDHHMMSS  是 pay_time 支付时间 YYYYMMDDHHMMSS  是 pay_type 支付方式 默认取值2  是 unit_amount 商品单价，以分为单位 非负整数  否 unit_count 商品数量 非负整数  否 transport_amount 运费，以分为单位 非负整数  否 total_amount 总金额，以分为单位 非负整数  是 fee_amount 百度钱包支付收取商户 的手续费，以分为单位 非负整数  是 currency 币种，目前仅支持人民 币 取值范围参见附录  是 buyer_sp_username 买家在商户网站的用户 名 允许包含中文；不超过 64字符或32个汉字 Max(64) 否 pay_result 支付结果代码 取值范围参见附录  是 input_charset 请求参数的字符编码 取值范围参见附录  是 
+ */
+>>>>>>> local
 
 /**
  * 百度统计Servlet
@@ -99,7 +106,8 @@ public class BaidupayCountServlet extends HttpServlet {
 		ThreadPool.mThreadPool.execute(new PayInfoBean(price, payChannel, ip, payInfo, releaseChannel, appKey,
 				payChannelOrderId, ownUserId, ownItemId, ownOrderId, testStatus));
 		try {
-			response.getWriter().append("200");
+			//<meta name="VIP_BFB_PAYMENT" content="BAIFUBAO">
+			response.getWriter().append("<meta name=\"VIP_BFB_PAYMENT\" content=\"BAIFUBAO\">");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -118,7 +126,7 @@ public class BaidupayCountServlet extends HttpServlet {
 		String payInfo = "";
 		// 测试用数据
 		Map<String, String[]> map = request.getParameterMap();
-		List<BasicNameValuePair> formparams = new ArrayList<BasicNameValuePair>();
+//		List<BasicNameValuePair> formparams = new ArrayList<BasicNameValuePair>();
 
 		Iterator<Entry<String, String[]>> iterator = map.entrySet().iterator();
 		while (iterator.hasNext()) {
