c2096963bdff98b25981cdbb6bc3227dbc79c909
soybean217 <13565644@qq.com>
2016-07-09 13:49:42 +0800

    initial

---
+++ cmpp-mt/src/SMSActiveTest.java
@@ -0,0 +1,109 @@
+/*
+ * Created on 2006-11-15
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ * 用于发送网关连接测试包。
+ */
+import comsd.commerceware.cmpp.*;
+import comsd.commerceware.cmpp.CMPP;
+import java.lang.*;
+import java.io.*;
+import com.xiangtone.util.FormatSysTime;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class SMSActiveTest implements Runnable {
+	CMPP p = null;//new CMPP();
+  	//public static conn_desc con = new conn_desc();
+  	public conn_desc con ;//= new conn_desc();
+  	cmppe_login cl = new cmppe_login();
+  	CMPPSingleConnect cmppcon ;//= CMPPSingleConnect.getInstance();
+  	
+  	public SMSActiveTest() 
+  	{		
+  						//p = new CMPP();
+  		//instance2 = CMPPSingleConnect_fj.getInstance();
+  		//con = instance2.con;		
+  					p = new CMPP();
+  					cmppcon = CMPPSingleConnect.getInstance();
+  					con = cmppcon.con;
+  					
+  	}
+  	/**
+  	*
+  	*
+  	*/
+  	public void run()
+  	{
+  		//////////////////日志记录/////////////
+  		/*
+	 		Logger myLogger = Logger.getLogger("MsgSendLogger");
+	  	Logger mySonLogger = Logger.getLogger("myLogger.mySonLogger");
+    	PropertyConfigurator.configure("log4j.properties");
+			*/
+	 		//////////////////////////////////////
+  		try
+  		{
+  			int i = 0;
+  				while(true)
+  				{
+  					System.out.println("-------send active test -------");
+  					//if(cmppcon == null )
+  					//{
+  						//cmppcon = CMPPSingleConnect.getInstance();
+  					//}
+                	try
+                	{	
+                		p.cmpp_active_test(cmppcon.con);
+                		Thread.currentThread().sleep(3000);
+                		//i++;
+                		/*
+                		if(i%6 == 0){
+                			myLogger.info(FormatSysTime.getCurrentTimeA() + "ActiveTest Msg");
+                		}
+                		if(i == 90000){
+                			i = 0;	
+                		}
+                		*/
+                	}
+                	catch(Exception e)
+                	{
+                		Logger myLogger = Logger.getLogger("MsgSendLogger");
+	  								Logger mySonLogger = Logger.getLogger("myLogger.mySonLogger");
+    								PropertyConfigurator.configure("log4j.properties");
+                		myLogger.info(FormatSysTime.getCurrentTimeA() + "testActive exception msg--Exception:" + e.toString());
+
+                		System.out.println(e.toString());
+                		System.out.println("重新连接...");
+    		    	    p.cmpp_disconnect_from_ismg(con);
+    		    	    cmppcon.destroy();
+  								
+    		    	    //cmppcon =null;
+    		    	    try{
+    		    	    	Thread.currentThread().sleep(10 * 1000);
+    		    	    	cmppcon = CMPPSingleConnect.getInstance(); //重连
+  									con = cmppcon.con;
+  									myLogger.info(FormatSysTime.getCurrentTimeA() + "平台0进行重新连接:" + e.toString());
+    		    	    }catch(Exception e1){
+    		    	    		
+    		    	    }
+    		    	    //cmppcon = CMPPSingleConnect.getInstance(); //重连
+                	}
+  					
+  			}
+  			
+  		}
+  		catch(Exception e)
+  		{
+  			System.out.println(e.toString());
+  		}	
+  	}	 	
+}
---
+++ cmpp-mt/src/ServerRestart.java
@@ -0,0 +1,15 @@
+//package platformalarm;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+public class ServerRestart {
+	public void Restart(){
+		try{
+			Runtime r = Runtime.getRuntime();
+			Process p = r.exec("java sms_public");
+		}catch(Exception e){
+			System.out.println(e.toString());
+		}
+	}
+		
+}
