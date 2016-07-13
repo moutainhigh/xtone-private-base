8c46ed4834cdc5a88ad1cfbc5bf534323e5bcd95
soybean217 <13565644@qq.com>
2016-07-13 22:21:00 +0800

    清理文件

--- cmpp-mt/src/org/x/CMPPSingleConnect.java
+++ cmpp-mt/src/org/x/CMPPSingleConnect.java
@@ -12,42 +12,44 @@ package org.x;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-import comsd.commerceware.cmpp.*;
 import comsd.commerceware.cmpp.CMPP;
-import java.lang.*;
-import java.io.*;
+import comsd.commerceware.cmpp.cmppe_login;
+import comsd.commerceware.cmpp.conn_desc;
+
 public class CMPPSingleConnect {
-	private  static CMPPSingleConnect cmppcon = null;
-    private  CMPP p = new CMPP();
-  	public   static conn_desc con = new conn_desc();
-  	private  cmppe_login cl = new cmppe_login();
-  	private CMPPSingleConnect(){
-  		connectIsmg();
-  	}
-  	public static synchronized CMPPSingleConnect getInstance(){
-  		if(cmppcon == null){
-  			cmppcon = new CMPPSingleConnect();		
-  		}
-  		return cmppcon;
-  	}
-  	
-  	private void connectIsmg(){
-  		try{
-  			System.out.println("��½����:" + SMSIsmgInfo.qw_ismg_ip);
-  			p.cmpp_connect_to_ismg(SMSIsmgInfo.qw_ismg_ip,SMSIsmgInfo.qw_ismg_port,con);
-  			cl.set_icpid(SMSIsmgInfo.qw_icpID);
-  			cl.set_auth(SMSIsmgInfo.qw_icpShareKey);
-  			cl.set_version((byte)0x30);
-  			cl.set_timestamp(1111101020);
-  			p.cmpp_login(con,cl);
-  		}catch(Exception e){
-  			System.out.println("err:login ismg failed! --CMPP_receive.java");
-    		System.out.println(e.toString());
-  		}
-  	}
-  	synchronized public static void destroy()
-	{
+	private static CMPPSingleConnect cmppcon = null;
+	private CMPP p = new CMPP();
+	public static conn_desc con = new conn_desc();
+	private cmppe_login cl = new cmppe_login();
+
+	private CMPPSingleConnect() {
+		connectIsmg();
+	}
+
+	public static synchronized CMPPSingleConnect getInstance() {
+		if (cmppcon == null) {
+			cmppcon = new CMPPSingleConnect();
+		}
+		return cmppcon;
+	}
+
+	private void connectIsmg() {
+		try {
+			System.out.println("connectIsmg:" + SMSIsmgInfo.qw_ismg_ip);
+			p.cmpp_connect_to_ismg(SMSIsmgInfo.qw_ismg_ip, SMSIsmgInfo.qw_ismg_port, con);
+			cl.set_icpid(SMSIsmgInfo.qw_icpID);
+			cl.set_auth(SMSIsmgInfo.qw_icpShareKey);
+			cl.set_version((byte) 0x30);
+			cl.set_timestamp(1111101020);
+			p.cmpp_login(con, cl);
+		} catch (Exception e) {
+			System.out.println("err:login ismg failed! --CMPP_receive.java");
+			System.out.println(e.toString());
+		}
+	}
+
+	synchronized public static void destroy() {
 		System.out.println("destory connect instance.......");
-		cmppcon =null;
+		cmppcon = null;
 	}
 }
